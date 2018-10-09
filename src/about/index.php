d35ee085dff26f19cfb5cb03f5a38b08386f7370
Elijahboyo <43387178+Elijahboyo@users.noreply.github.com>
2018-10-09 11:42:06 -0700

    about page

--- src/about/index.php
+++ src/about/index.php
@@ -14,9 +14,11 @@
 			<p style="padding: 20px 0px;" class="lead">
 				RIOT stands for Rocklin Institute of Technology. RIOT is a high school technology class where students get to have firsthand experiences dealing with computers. The source for this website can be found 
 				<em>
-					<a href="https://github.com/JustinRisney/RIOTWebDev" target="_blank">here</a>
+					<a href="https://github.com/JustinRisney/RIOTWebDev" target="_blank">here</a>	
 				</em>
-				.
+				<p style="padding: 20px 0px;" class="lead">
+				RIOT forms a professional IT environment that reflects a real IT company and acts as a company. We have multiple groups in our company to be more efficient to our clients. These teams are Web Development, Robotics and Education, Management and Development, Programming, and Cyber Security. As a IT company but as a school we train and complete projects to our clients wants and enforce the ideas and concept of agile, scrum, and trello. We use sprints and daily stand up meetings to ensure we meet our deadlines.
+
 			</p>
 		</div>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
